6eddb8c7f238792f8b6776f9f868d9bf77618541
Yevhen Triukhan <yevhen.triukhan@gmail.com>
2020-08-19 15:08:11 +0300

    Added empty project.

---
+++ NYTimesViewer/NYTimesViewer/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NYTimesViewer
+//
+//  Created by Yevhen Triukhan on 19.08.2020.
+//  Copyright © 2020 Yevhen Triukhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
